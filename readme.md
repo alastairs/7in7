665b1d972e6eff238039a093a5c8f207acaa0945
Alastair Smith <github@alastairsmith.me.uk>
2011-08-20 16:10:54 +0100

    Ruby Day 3 exercise.  Can't get it to work though - method_missing doesn't seem to be called at all\!

--- readme.md
+++ readme.md
@@ -3,3 +3,4 @@ This repository tracks my progress through Bruce Tate's excellent book, _Seven L
  * **14/08/11:** Ruby Day 1
  * **15/08/11:** Refactor the day 1 exercises. Ruby Day 2.
  * **16/08/11:** Finish Ruby Day 2 exercises.
+ * **20/08/11:** Ruby Day 3. Can't get the exercise to work, though :-(
--- ruby/day3/acts_as_csv_class.rb
+++ ruby/day3/acts_as_csv_class.rb
@@ -1,4 +1,28 @@
 module ActsAsCsv
+
+	class CsvRow
+		def self.method_missing name, *args
+			puts "CsvRow.method_missing called!"
+			if (@row.has_key? name)
+				@row[name]
+			end
+		end
+
+		def initialize(headers = [], row = [])
+			@row = {}
+			if headers.size == row.size
+				for i in 0..headers.size do
+					if headers[i].nil? == false
+						@row[headers[i]] = row[i]
+					end
+				end
+			end
+		end
+		
+		def self.to_s
+			@row.each {|header, value| header + " => " + value}
+		end
+	end
 	
 	def self.included(base)
 		base.extend ClassMethods
@@ -14,13 +38,23 @@ module ActsAsCsv
 
 		def read
 			@csv_contents = []
+			@csv_rows = []
+			
 			filename = self.class.to_s.downcase + '.txt'
 			file = File.new(filename)
 			@headers = file.gets.chomp.split(', ')
 
 			file.each do |row|
-				@csv_contents << row.chomp.split(', ') 
+				parsed_row = row.chomp.split(', ')
+				@csv_contents << parsed_row
+				@csv_rows << CsvRow.new(@headers, parsed_row)
 			end
+
+			@csv_rows.each {|row| puts row.to_s}
+		end
+		
+		def each &block
+			@csv_rows.each &block 
 		end
 		
 		attr_accessor :headers, :csv_contents
@@ -38,6 +72,5 @@ class RubyCsv
 	acts_as_csv
 end
 
-m = RubyCsv.new
-puts m.headers.inspect
-puts m.csv_contents.inspect
+csv = RubyCsv.new
+puts csv.each {|row| puts row.FirstName}
