85a448ec81596b7f75fe82ee0a7662818509c8c4
Alastair Smith <github@alastairsmith.me.uk>
2011-08-20 14:41:33 +0100

    Version 3 of the CSV file class, which now uses modules in its metaprogramming to add new behaviours to the class by importing the module and calling a macro.  No inheritance needed\!

--- ruby/day3/acts_as_csv_class.rb
+++ ruby/day3/acts_as_csv_class.rb
@@ -1,33 +1,43 @@
-class ActsAsCsv
-	def self.acts_as_csv
-		define_method 'read' do
-			file = File.new(self.class.to_s.downcase + '.txt')
-			@headers = file.gets.chomp.split(', ')
+module ActsAsCsv
+	
+	def self.included(base)
+		base.extend ClassMethods
+	end
 
-			file.each do |row| 
-				@result << row.chomp.split(', ') 
-			end
-		end
-
-		define_method "headers" do
-			@headers
-		end
-
-		define_method "csv_contents" do
-			@result
-		end
-
-		define_method "initialize" do
-			@result = []
-			read
+	module ClassMethods
+		def acts_as_csv
+			include InstanceMethods
 		end
 	end
+
+	module InstanceMethods
+
+		def read
+			@csv_contents = []
+			filename = self.class.to_s.downcase + '.txt'
+			file = File.new(filename)
+			@headers = file.gets.chomp.split(', ')
+
+			file.each do |row|
+				@csv_contents << row.chomp.split(', ') 
+			end
+		end
+		
+		attr_accessor :headers, :csv_contents
+
+		def initialize
+			read
+		end
+
+	end
+
 end
 
-class RubyCsv < ActsAsCsv
+class RubyCsv
+	include ActsAsCsv
 	acts_as_csv
 end
 
 m = RubyCsv.new
 puts m.headers.inspect
-puts m_csv_contents.inspect
+puts m.csv_contents.inspect
