6cc3d29e6e7a0ba4cb89e67aff2641362d83db42
Alastair Smith <github@alastairsmith.me.uk>
2011-08-21 17:18:51 +0100

    Prolog day 1 first self-study exercise: books and authors knowledge base.

---
+++ prolog/day1/books.pl
@@ -0,0 +1,8 @@
+wrote(george_r_r_martin, game_of_thrones).
+wrote(j_r_r_tolkien, the_hobbit).
+wrote(j_r_r_tolkien, the_fellowship_of_the_ring).
+wrote(j_r_r_tolkien, the_two_towers).
+wrote(j_r_r_tolkien, the_return_of_the_king).
+wrote(jane_austen, pride_and_prejudice).
+wrote(jane_austen, sense_and_sensibility).
+wrote(jane_austen, emma).
