6d38c7346baf97ec92726d919df7272cccc74954
Alastair Smith <github@alastairsmith.me.uk>
2011-08-21 17:26:28 +0100

    Prolog day 1 second self-study exercise: musicians, instruments and genres knowledge base.

---
+++ prolog/day1/musicians.pl
@@ -0,0 +1,27 @@
+plays(maxim_vengerov, violin).
+plays(joshua_bell, violin).
+plays(vladimir_ashkenazy, piano).
+plays(sergio_azzolini, bassoon).
+plays(ursula_levaux, bassoon).
+plays(stephen_isserlis, cello).
+plays(jimi_hendrix, guitar).
+plays(matt_bellamy, guitar).
+plays(matt_bellamy, piano).
+plays(hugh_laurie, piano).
+plays(oscar_peterson, piano).
+plays(miles_davis, trumpet).
+plays(charlie_parker, saxophone).
+
+musician_genres(maxim_vengerov, classical).
+musician_genres(joshua_bell, classical).
+musician_genres(vladimir_ashkenazy, classical).
+musician_genres(sergio_azzolini, classical).
+musician_genres(ursula_levaux, classical).
+musician_genres(stephen_isserlis, classical).
+musician_genres(jimi_hendrix, rock).
+musician_genres(matt_bellamy, rock).
+musician_genres(hugh_laurie, blues).
+musician_genres(oscar_peterson, jazz).
+musician_genres(miles_davis, jazz).
+musician_genres(charlie_parker, jazz).
+
